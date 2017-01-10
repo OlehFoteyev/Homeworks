7d5c5e3b9021a984ae12f4182a91a37af53f13af
Oleh Foteyev <www.foteev@mail.ru>
2017-01-10 19:26:54 +0200

    array

--- 15.12.2016/arrays_loops_tasks/12.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-for ($num = 1000, $i = 0; $num > 50; $num /= 2, $i++);
-
-echo $i.'<br>';
-echo $num;
--- 15.12.2016/arrays_loops_tasks/8.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo '<pre>';
-$str ="";
-$arr =array (1,2,3,4,5,6,7,8,9,10);
-foreach($arr as $name)
-    $str .= $name;
-echo $str;
-echo '</pre>';
